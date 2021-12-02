c9d9b3d49def5d042e059547968a4660f6d27ba5
Daniel Dugovic <ddugovic@users.noreply.github.com>
2021-12-02 07:32:31 -0600

    Format using vim-autopep8

--- aoc_dan/aoc2021/q01.py
+++ aoc_dan/aoc2021/q01.py
@@ -4,22 +4,29 @@ https://adventofcode.com/2021/day/1
 """
 from aocd import data
 
-count1 = 0
-count2 = 0
-lastdepth = 0
-lastsum = 0
-window = [0, 0, 0]
-for step, depth in enumerate(data.splitlines()):
-    depth = int(depth)
-    if step >= 1 and depth > lastdepth:
-        count1 = count1 + 1
-    lastdepth = depth
+def solve():
+    """How many measurements are larger than the previous measurement?
+    Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?"""
 
-    window = window[1:]
-    window.append(depth)
-    if step >= 3 and sum(window) > lastsum:
-        count2 = count2 + 1
-    lastsum = sum(window)
+    count1 = 0
+    count2 = 0
+    lastdepth = 0
+    lastsum = 0
+    window = [0, 0, 0]
+    for step, depth in enumerate(data.splitlines()):
+        depth = int(depth)
+        if step >= 1 and depth > lastdepth:
+            count1 = count1 + 1
+        lastdepth = depth
 
-print("part a:", count1)
-print("part b:", count2)
+        window = window[1:]
+        window.append(depth)
+        if step >= 3 and sum(window) > lastsum:
+            count2 = count2 + 1
+        lastsum = sum(window)
+
+    print("part a:", count1)
+    print("part b:", count2)
+
+if __name__ == "__main__":
+    solve()
--- aoc_dan/aoc2021/q02.py
+++ aoc_dan/aoc2021/q02.py
@@ -4,28 +4,36 @@ https://adventofcode.com/2021/day/2
 """
 from aocd import data
 
-distance = 0
-depth = 0
-for line in data.splitlines():
-    command = line.split()
-    if command[0] == "forward":
-        distance += int(command[1])
-    elif command[0] == "down":
-        depth += int(command[1])
-    elif command[0] == "up":
-        depth -= int(command[1])
-print("part a:", distance * depth)
+def solve():
+    """Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 
-aim = 0
-distance = 0
-depth = 0
-for line in data.splitlines():
-    command = line.split()
-    if command[0] == "forward":
-        distance += int(command[1])
-        depth += aim * int(command[1])
-    elif command[0] == "down":
-        aim += int(command[1])
-    elif command[0] == "up":
-        aim -= int(command[1])
-print("part b:", distance * depth)
+    Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?"""
+
+    distance = 0
+    depth = 0
+    for line in data.splitlines():
+        command = line.split()
+        if command[0] == "forward":
+            distance += int(command[1])
+        elif command[0] == "down":
+            depth += int(command[1])
+        elif command[0] == "up":
+            depth -= int(command[1])
+    print("part a:", distance * depth)
+
+    aim = 0
+    distance = 0
+    depth = 0
+    for line in data.splitlines():
+        command = line.split()
+        if command[0] == "forward":
+            distance += int(command[1])
+            depth += aim * int(command[1])
+        elif command[0] == "down":
+            aim += int(command[1])
+        elif command[0] == "up":
+            aim -= int(command[1])
+    print("part b:", distance * depth)
+
+if __name__ == "__main__":
+    solve()
